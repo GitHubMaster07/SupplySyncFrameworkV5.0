ce2f9e57119c5d188d20c6df289ace6c722e1f7c
Sergei <sergeiqaengineer@yandex.com>
2024-05-01 23:45:51 -0500

    New automation added (date 05.01.2024)

---
+++ src/test/java/pages/SupplySyncRegionPage.java
@@ -0,0 +1,32 @@
+package pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class SupplySyncRegionPage {
+    public SupplySyncRegionPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy (xpath = "//span[text()='Regions']")
+    public WebElement regionNavigate;
+
+    @FindBy (xpath = "//button[@class='sc-jJoQJp ieRzNh MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButtonBase-root sc-eGRUor dTmEKg sc-gIDmLj fpapDT sc-gLDmcm cdpqXd']")
+    public WebElement createRegion;
+
+    @FindBy(name = "name")
+    public WebElement regionName;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement createRegionBtn;
+
+    @FindBy (xpath = "(//button[@class='sc-jJoQJp ieRzNh MuiButtonBase-root sc-hiCibw kVxtLs MuiIconButton-root MuiIconButton-sizeMedium'])[2]")
+    public WebElement regionEditDeleteButton;
+
+    @FindBy (xpath = "(//li[@class='sc-jJoQJp ieRzNh MuiMenuItem-root MuiMenuItem-gutters MuiButtonBase-root sc-jOxtWs iooOuN sc-hcupDf kXRKGO'])[5]")
+    public WebElement regionDeleteButton;
+
+    @FindBy(css = "button[class='sc-jJoQJp ieRzNh MuiButton-root MuiButton-contained MuiButton-containedError MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButtonBase-root sc-eGRUor hHAfIf sc-gIDmLj fpaoOd']")
+    public WebElement regionConfirmDeleting;
+}
+
